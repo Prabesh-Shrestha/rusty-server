087f54a18f54033ada631c686e1b72c0980ac6cc
Prabesh Shrestha <creatorworld329@gmail.com>
2022-10-13 17:16:37 +0545

    distributed in modules

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,7 +6,7 @@ use std::net::TcpListener;
 fn main() {
     const PORT: &str = "7878";
     let listener = TcpListener::bind(String::from("127.0.0.1:")+ PORT).unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = thread_pool::ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         pool.execute(|| {
--- src/lib.rs
+++ src/lib.rs
@@ -1,113 +1,134 @@
-use std::sync::mpsc;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
+pub mod thread_pool {
+    use std::sync::mpsc;
+    use std::sync::Arc;
+    use std::sync::Mutex;
+    use std::thread;
 
-use std::net::TcpStream;
+    type Job = Box<dyn FnOnce() + Send + 'static>;
 
-// use listener::*;
-use std::fs;
-use std::io::prelude::*;
-use std::collections::HashMap;
+    enum ThreadState {
+        Doing(Job),
+        Terminate,
+    }
+    pub struct Worker {
+        id: usize,
+        thread: Option<thread::JoinHandle<()>>,
+    }
 
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-enum ThreadState {
-    Doing(Job),
-    Terminate,
-}
-pub struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<ThreadState>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let state = receiver.lock().unwrap().recv().unwrap();
-            match state {
-                ThreadState::Doing(job) => {
-                    println!("Worder {}, got a job; executing.", id);
-                    job();
+    impl Worker {
+        fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<ThreadState>>>) -> Worker {
+            let thread = thread::spawn(move || loop {
+                let state = receiver.lock().unwrap().recv().unwrap();
+                match state {
+                    ThreadState::Doing(job) => {
+                        println!("Worder {}, got a job; executing.", id);
+                        job();
+                    }
+                    ThreadState::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+                        break;
+                    }
                 }
-                ThreadState::Terminate => {
-                    println!("Worker {} was told to terminate.", id);
-                    break;
-                }
+            });
+            Worker {
+                id,
+                thread: Some(thread),
             }
-        });
-        Worker {
-            id,
-            thread: Some(thread),
         }
     }
-}
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<ThreadState>,
-}
+    pub struct ThreadPool {
+        workers: Vec<Worker>,
+        sender: mpsc::Sender<ThreadState>,
+    }
 
-impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
-        let mut workers = Vec::with_capacity(size);
-        for id in 0..size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
+    impl ThreadPool {
+        pub fn new(size: usize) -> ThreadPool {
+            assert!(size > 0);
+            let (sender, receiver) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+            let mut workers = Vec::with_capacity(size);
+            for id in 0..size {
+                workers.push(Worker::new(id, Arc::clone(&receiver)));
+            }
+            ThreadPool { workers, sender }
         }
-        ThreadPool { workers, sender }
-    }
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
-        let job = Box::new(f);
-        self.sender.send(ThreadState::Doing(job)).unwrap();
-    }
-}
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        for _ in &self.workers {
-            self.sender.send(ThreadState::Terminate).unwrap();
+        pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+        {
+            let job = Box::new(f);
+            self.sender.send(ThreadState::Doing(job)).unwrap();
         }
-        for worker in &mut self.workers {
-            println!("shutting down worker {}", worker.id);
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
+    }
+    impl Drop for ThreadPool {
+        fn drop(&mut self) {
+            for _ in &self.workers {
+                self.sender.send(ThreadState::Terminate).unwrap();
+            }
+            for worker in &mut self.workers {
+                println!("shutting down worker {}", worker.id);
+                if let Some(thread) = worker.thread.take() {
+                    thread.join().unwrap();
+                }
             }
         }
     }
 }
 
-pub fn check_req(buffer: [u8; 1024]) -> (String, String) {
-    let mut req_hash: HashMap<&str, &str> = HashMap::new();
-    let buffer = String::from_utf8_lossy(&buffer[..]);
-    req_hash.insert("GET /sleep HTTP/1.1\r\n", "public/index.html");
-    req_hash.insert("GET /sleep HTTP/1.1\r\n", "public/sleep.html");
-    for &req in req_hash.keys(){
-        if buffer.starts_with(req) {
-            return (req.to_string(), req_hash.get(req).unwrap().to_string())
-        }
+pub mod server {
+    struct Server {
+        req_hash: HashMap<String, String>,
     }
-    ("HTTP/1.1 404 NOT FOUND".to_string(), "public/404.html".to_string())
+
+    use std::collections::HashMap;
+    use std::fs;
+    use std::io::prelude::*;
+    use std::net::TcpStream;
+
+    pub fn check_req(buffer: [u8; 1024]) -> (String, String) {
+        let mut req_hash: HashMap<String, String> = HashMap::new();
+        let buffer = String::from_utf8_lossy(&buffer[..]);
+        req_hash.insert(
+            "GET /sleep HTTP/1.1\r\n".to_string(),
+            "public/index.html".to_string(),
+        );
+        req_hash.insert(
+            "GET /sleep HTTP/1.1\r\n".to_string(),
+            "public/sleep.html".to_string(),
+        );
+        for req in req_hash.keys() {
+            if buffer.starts_with(req) {
+                return (
+                    req.to_string(),
+                    match req_hash.get(&req.clone()) {
+                        Some(addr) => addr.to_string(),
+                        None => {
+                            // handle error
+                            panic!("error while handleing {}", req);
+                        }
+                    },
+                );
+            }
+        }
+        (
+            "HTTP/1.1 404 NOT FOUND".to_string(),
+            "public/404.html".to_string(),
+        )
+    }
+
+    pub fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        let (status_line, filename) = check_req(buffer);
+        let content = fs::read_to_string(filename).unwrap();
+        let responce = format!(
+            "{}\r\nContent-Lenght: {}\r\n\r\n{}",
+            status_line,
+            content.len(),
+            content
+        );
+        stream.write(responce.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
-
-pub fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let (status_line, filename) = check_req(buffer);
-    let content = fs::read_to_string(filename).unwrap();
-    let responce = format!(
-        "{}\r\nContent-Lenght: {}\r\n\r\n{}",
-        status_line,
-        content.len(),
-        content
-    );
-    stream.write(responce.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-
